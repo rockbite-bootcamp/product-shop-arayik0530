83ead6d83bd5247480bdd06979badb19457e842d
arayik0530 <arayik0530@gmail.com>
2020-08-06 14:55:00 +0400

    refactored

--- src/com/company/ShopImpl.java
+++ src/com/company/ShopImpl.java
@@ -12,11 +12,15 @@ import java.util.Set;
  */
 public class ShopImpl implements IShop {
 
+    //items taken from buyers
+    private List<Item> itemsInCashDesk;
+
     //products to be sold in the shop
     private List<Product> products;
 
     public ShopImpl() {
         this.products = new ArrayList<>();
+        this.itemsInCashDesk = new ArrayList<>();
     }
 
     @Override
@@ -28,35 +32,41 @@ public class ShopImpl implements IShop {
     public void buy(final Player buyer, final Product product) throws IOException, ClassNotFoundException {
         //deep clone of Product's payload
         List<Item> copyOfPayloadItems = new ArrayList<>();
-        for (Item payloadItem: product.getPayload()) {
+        for (Item payloadItem : product.getPayload()) {
             copyOfPayloadItems.add(payloadItem.deepClone());
         }
 
         //deep clone of Player's items
         List<Item> copyOfPlayerItems = new ArrayList<>();
-        for (Item playerItem: buyer.getItems()) {
+        for (Item playerItem : buyer.getItems()) {
             copyOfPlayerItems.add(playerItem.deepClone());
+        }
+
+        //deep clone of items in the cash desk
+        List<Item> copyOfCashDesk = new ArrayList<>();
+        for (Item item : this.itemsInCashDesk) {
+            copyOfCashDesk.add(item.deepClone());
         }
 
         //temp list is added to avoid concurrentModificationException
         List<Item> tempItemsToRemove = new ArrayList<>();
 
-        for (Item costItem: product.getCost() ) {
+        for (Item costItem : product.getCost()) {
             int count = 0;
-            for (Item playerItem: buyer.getItems()) {
-                if(costItem.getId() == playerItem.getId()){
+            for (Item playerItem : buyer.getItems()) {
+                if (costItem.getId() == playerItem.getId()) {
                     ++count;
-                    if(playerItem.getCount() - costItem.getCount() >= 0){
+                    if (playerItem.getCount() - costItem.getCount() >= 0) {
                         playerItem.setCount(playerItem.getCount() - costItem.getCount());
-                        if(playerItem.getCount() == 0){
+                        if (playerItem.getCount() == 0) {
                             tempItemsToRemove.add(playerItem);
                         }
-                    }
-                    else{
+                    } else {
 
                         //rollback
                         product.setPayload(copyOfPayloadItems);
                         buyer.setItems(copyOfPlayerItems);
+                        this.itemsInCashDesk = copyOfCashDesk;
                         //rollback
 
                         throw new TransactionFailedException("The player does not have enough count of required item " +
@@ -64,10 +74,11 @@ public class ShopImpl implements IShop {
                     }
                 }
             }
-            if(count == 0){
+            if (count == 0) {
                 //rollback
                 product.setPayload(copyOfPayloadItems);
                 buyer.setItems(copyOfPlayerItems);
+                this.itemsInCashDesk = copyOfCashDesk;
                 //rollback
                 throw new TransactionFailedException("The player does not have required item" +
                         costItem);
@@ -81,13 +92,12 @@ public class ShopImpl implements IShop {
         //temp set is added to avoid concurrentModificationException
         Set<Item> tempItemsToAdd = new HashSet<>();
 
-        for (Item payloadItem: product.getPayload()) {
+        for (Item payloadItem : product.getPayload()) {
 
-            for (Item playerItem: buyer.getItems()) {
-                if(payloadItem.getId() == playerItem.getId()){
+            for (Item playerItem : buyer.getItems()) {
+                if (payloadItem.getId() == playerItem.getId()) {
                     playerItem.setCount(playerItem.getCount() + payloadItem.getCount());
-                }
-                else{
+                } else {
                     tempItemsToAdd.add(payloadItem);
                 }
             }
@@ -98,6 +108,30 @@ public class ShopImpl implements IShop {
 
         //removing sold product from the shop
         this.products.remove(product);
+
+        //adding cost payload to the cash desk
+
+        if (itemsInCashDesk.size() == 0) {
+            itemsInCashDesk.addAll(product.getCost());
+        } else {
+            //temp set is added to avoid concurrentModificationException
+            Set<Item> tempItemsToAddInDesk = new HashSet<>();
+
+            for (Item costItem : product.getCost()) {
+
+                for (Item deskItem : itemsInCashDesk) {
+
+                    if (costItem.getId() == deskItem.getId()) {
+                        deskItem.setCount(costItem.getCount() + deskItem.getCount());
+                    } else {
+                        tempItemsToAddInDesk.add(costItem);
+                    }
+
+                }
+            }
+
+            this.itemsInCashDesk.addAll(tempItemsToAddInDesk);
+        }
     }
 
     @Override
@@ -113,7 +147,8 @@ public class ShopImpl implements IShop {
     @Override
     public String toString() {
         return "Shop{" +
-                "products=" + products +
+                "itemsInCashDesk=" + itemsInCashDesk +
+                ", products=" + products +
                 '}';
     }
 }
